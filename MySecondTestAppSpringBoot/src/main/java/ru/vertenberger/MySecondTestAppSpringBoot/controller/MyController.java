d093cc57885d4051f37430091ab36c28637a0054
eva <evajjikk@gmail.com>
2024-10-20 23:20:56 +0500

    lr 3

--- MySecondTestAppSpringBoot/src/main/java/ru/vertenberger/MySecondTestAppSpringBoot/controller/MyController.java
+++ MySecondTestAppSpringBoot/src/main/java/ru/vertenberger/MySecondTestAppSpringBoot/controller/MyController.java
@@ -1,7 +1,9 @@
 package ru.vertenberger.MySecondTestAppSpringBoot.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -10,57 +12,68 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.vertenberger.MySecondTestAppSpringBoot.exception.ValidationFailedException;
 import ru.vertenberger.MySecondTestAppSpringBoot.exception.UnsupportedCodeException;
-import ru.vertenberger.MySecondTestAppSpringBoot.model.Request;
-import ru.vertenberger.MySecondTestAppSpringBoot.model.Response;
+import ru.vertenberger.MySecondTestAppSpringBoot.model.*;
+import ru.vertenberger.MySecondTestAppSpringBoot.service.ModifyResponseService;
 import ru.vertenberger.MySecondTestAppSpringBoot.service.ValidationService;
+import ru.vertenberger.MySecondTestAppSpringBoot.util.DateTimeUtil;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Slf4j
 @RestController
 public class MyController {
 
     private final ValidationService validationService;
 
+    private final ModifyResponseService modifyResponseService;
+
     @Autowired
-    public MyController(ValidationService validationService) {
+    public MyController(ValidationService validationService,
+                        @Qualifier("ModifySystemTimeResponseService") ModifyResponseService modifyResponseService) {
         this.validationService = validationService;
+        this.modifyResponseService = modifyResponseService;
     }
 
     @PostMapping(value = "/feedback")
     public ResponseEntity<Response> feedback(@Valid @RequestBody Request request,
                                              BindingResult bindingResult) {
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MMdd'T'MM:mm:ss.SSS'Z'");
+        log.info("request: {}", request);
 
         Response response = Response.builder()
                 .uid(request.getUid())
                 .operationUid(request.getOperationUid())
-                .systemTime(simpleDateFormat.format(new Date()))
-                .code("success")
-                .errorCode("")
-                .errorMessage("")
+                .systemTime(DateTimeUtil.getCustomFormat().format(new Date()))
+                .code(Codes.SUCCESS)
+                .errorCode(ErrorCodes.EMPTY)
+                .errorMessage(ErrorMessages.EMPTY)
                 .build();
         try {
             request.checkUid();
             validationService.isValid(bindingResult);
         } catch (UnsupportedCodeException e) {
-            response.setCode("failed");
-            response.setErrorCode("UnsupportedCodeException");
-            response.setErrorMessage(e.getMessage());
+            log.error("Произошла непредвиденная ошибка {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.UNSUPPORTED_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.UNSUPPORTED);
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         } catch (ValidationFailedException e) {
-            response.setCode("failed");
-            response.setErrorCode("ValidationException");
-            response.setErrorMessage("Ошибка валидации!");
+            log.error("Ошибка валидации {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.VALIDATION_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.VALIDATION);
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            response.setCode("failed");
-            response.setErrorCode("UnknownException");
-            response.setErrorMessage("Произошла непредвиденная ошибка!");
+            log.error("Не поддерживаемая ошибка {}", e.getMessage());
+            response.setCode(Codes.FAILED);
+            response.setErrorCode(ErrorCodes.UNKNOWN_EXCEPTION);
+            response.setErrorMessage(ErrorMessages.UNKNOWN);
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        modifyResponseService.modify(response);
+        log.info("Ответ: {}", response);
+
+        return new ResponseEntity<>(modifyResponseService.modify(response), HttpStatus.OK);
     }
 }
 
--- MySecondTestAppSpringBoot/src/main/java/ru/vertenberger/MySecondTestAppSpringBoot/model/Request.java
+++ MySecondTestAppSpringBoot/src/main/java/ru/vertenberger/MySecondTestAppSpringBoot/model/Request.java
@@ -24,7 +24,7 @@ public class Request {
     @Size(max=32)
     private String operationUid;
 
-    private String systemName;
+    private Systems systemName;
 
     @NotBlank
     private String systemTime;
@@ -45,5 +45,19 @@ public class Request {
         }
     }
 
+    @Override
+    public String toString() {
+        return "{" +
+                "uid='" + uid + '\'' +
+                ", operationUid='" + operationUid + '\'' +
+                "systemName='" + systemName + '\'' +
+                "systemTime='" + systemTime + '\'' +
+                "source'" + source + '\'' +
+                "communicationId=" + communicationId +
+                "templateId=" + templateId +
+                "productCode=" + productCode +
+                "smsCode" + smsCode +
+                "}";
+    }
 }
 
